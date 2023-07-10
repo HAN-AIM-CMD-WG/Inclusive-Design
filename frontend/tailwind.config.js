474f43078022d25792f81a778622afb72688a6b0
jveldmaat <93977495+jveldmaat@users.noreply.github.com>
2023-07-10 20:13:43 +0200

    Update tailwind.config.js
    default kleuren neerzetten

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        colors: {
+            'HANROZE': 'PINK-700',
+            'HANROZELIGHT': 'PINK-600',
+            'HANGRIJS': 'GRAY-800',
+            'HANPAARS': 'PURPLE-600'
+        },
+        extend: {}
+    },
+    plugins: []
 }
