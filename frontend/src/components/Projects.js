05ab79189a53d17d30a7e3ed6ee4e4b4505629ee
jveldmaat <93977495+jveldmaat@users.noreply.github.com>
2023-07-10 22:59:38 +0200

    Create Projects.js
    Created component which mocks projects (for now). Gonna start building planes for the projects

---
+++ frontend/src/components/Projects.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Projects = () => {
+    const projects = [
+        {
+            title: 'Project 1',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 2',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 3',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 4',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 5',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 6',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 7',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 8',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        },
+        {
+            title: 'Project 9',
+            description: 'A brief description about Project 1.',
+            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
+        }
+]
+
+return(
+    
+)
+}
+
+export default Projects
