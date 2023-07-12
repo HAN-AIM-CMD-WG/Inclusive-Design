6f45ca2d1bf8ca9476231a4da5d2abf1cc5b39e0
jveldmaat <93977495+jveldmaat@users.noreply.github.com>
2023-07-12 21:07:53 +0200

    Update Projects.js
    herorganiseren

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,57 +1,35 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react'
 
-const Projects = () => {
-    const projects = [
-        {
-            title: 'Project 1',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 2',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 3',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 4',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 5',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 6',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 7',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 8',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        },
-        {
-            title: 'Project 9',
-            description: 'A brief description about Project 1.',
-            imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png'
-        }
-]
+const Projects = ({ theme, projects }) => {
+  const [filteredProjects, setFilteredProjects] = useState(projects)
 
-return(
-    
-)
+  useEffect(() => {
+    setFilteredProjects(
+      projects.filter(project => {
+        return ((!theme || theme === project.theme))
+      })
+    )
+  }, [theme])
+
+  return (
+    <div className="p-8 space-y-8 bg-gray-800">
+    <h2 className="text-3xl font-semibold text-center text-white" data-aos="fade-up">
+      Featured Projects
+    </h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" data-aos="fade-up">
+      {filteredProjects.map((project, index) => (
+        <div key={index} className="flex flex-col bg-white text-gray-900 rounded shadow-lg overflow-hidden">
+          <img src={project.imageUrl} alt={project.title} className="w-full h-64 object-cover"/>
+          <div className="p-6 space-y-4">
+            <h3 className="text-xl font-bold">{project.title}</h3>
+            <p className="text-gray-700">{project.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+  )
 }
 
 export default Projects
