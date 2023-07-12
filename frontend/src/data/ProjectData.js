d683a2dc76bfd8d44aabbd1690760f449523d171
jveldmaat <93977495+jveldmaat@users.noreply.github.com>
2023-07-12 21:08:02 +0200

    Create ProjectData.js
    ProjectData structuur aangemaakt

---
+++ frontend/src/data/ProjectData.js
@@ -0,0 +1,95 @@
+const projects = [
+  {
+    title: 'Project 1',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'entrust',
+    expertises: [
+      'python', 'react', 'photoshop'
+    ],
+    participants: 5
+  },
+  {
+    title: 'Project 2',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'core',
+    expertises: [
+      'java', 'html', 'photoshop'
+    ],
+    participants: 4
+
+  },
+  {
+    title: 'Project 3',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'entrust',
+    expertises: [
+      'designen', 'adobe'
+    ],
+    participants: 3
+  },
+  {
+    title: 'Project 4',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'core',
+    expertises: [
+      'java', 'html', 'photoshop'
+    ],
+    participants: 5
+  },
+  {
+    title: 'Project 5',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'entrust',
+    expertises: [
+      'java', 'html', 'photoshop'
+    ],
+    participants: 5
+  },
+  {
+    title: 'Project 6',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'core',
+    expertises: [
+      'java', 'html', 'photoshop'
+    ],
+    participants: 2
+  },
+  {
+    title: 'Project 7',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'entrust',
+    expertises: [
+      'java', 'html', 'photoshop'
+    ],
+    participants: 8
+  },
+  {
+    title: 'Project 8',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'core',
+    expertises: [
+      'java', 'html', 'photoshop'
+    ],
+    participants: 3
+  },
+  {
+    title: 'Project 9',
+    description: 'A brief description about Project 1.',
+    imageUrl: 'https://ngi.aimsites.nl/wp-content/uploads/2022/10/headerimage_ngi0_v4_8bit.png',
+    theme: 'entrust',
+    expertises: [
+      'java', 'html', 'photoshop'
+    ],
+    participants: 2
+  }
+]
+
+export default projects
