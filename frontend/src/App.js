eb60a720eda5b37a38b88b343733f5cbad9d4118
Troy98 <Gauvun@hotmail.com>
2023-07-14 17:07:39 +0200

    Profile component en tweede navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
